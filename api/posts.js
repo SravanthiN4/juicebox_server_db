75305449f8c488fca42ca67f9de1767f0dae51ef
SravanthiN4 <snedunoori44@gmail.com>
2022-04-09 10:20:19 -0500

    add posts code

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const postsRouter = express.Router();
-const {getAllPosts} = require('../db')
+const {getAllPosts, createPost} = require('../db')
+const {requireUser} = require('./util')
 
 postsRouter.use((req,res,next) => {
     console.log("A request has been made for ./posts");
@@ -14,5 +15,40 @@ postsRouter.get('/',async(req,res) => {
     });
 
 });
+
+postsRouter.post('/',requireUser, async(req,res,next) => {
+    const {title, content, tags = ""} = req.body;
+
+    const tagArr = tags.trim().split(/\s+/)
+    const postData  = {};
+    // only send the tags if there are some to send
+    if(tagArr.length) {
+        postData.tags = tagArr;
+    }
+    try {
+        postData.authorId = req.user.id
+        postData.title = title
+        postData.content = content
+
+        if(postData.length !== 0) {
+        const post = await createPost(postData);
+        console.log(post);
+        
+        res.send({ post })
+        }
+
+        else{
+
+        next({ 
+            name: 'PostNotAvailable', 
+            message: 'Post is not created'
+          });
+        }
+        
+    } catch ({ name, message }) {
+        next({ name, message });
+      }
+    
+})
 
 module.exports = postsRouter;

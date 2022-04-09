c71ad6604bd8603dc98fc31a668b9073dd9dd5f9
SravanthiN4 <snedunoori44@gmail.com>
2022-04-09 11:55:54 -0500

    heroku deployment

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 const { Client } = require('pg'); // imports the pg module
 const { client_encoding } = require('pg/lib/defaults');
 
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+//const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 /**
  * USER Methods
